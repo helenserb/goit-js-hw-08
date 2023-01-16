147472b1dde557e72f51d10dfce7707261544089
helenserb <serbinenkoifbb@gmail.com>
2023-01-16 15:25:30 +0200

    Second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
 
-player.on('timeupdate', onPlay);
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const time = data.seconds;
+    localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, time);
+  }, 1000)
+);
 
-function onPlay() {
-    throttle(function (data) {
-      const time = data.seconds;
-      localStorage.setItem('videoplayer - current - time', time);
-    }, 1000);
-}
+const time = +localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
+player.setCurrentTime(time);
