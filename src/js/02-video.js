97b4f66e6201496def209530bdd0cca97577145f
helenserb <serbinenkoifbb@gmail.com>
2023-01-16 12:17:21 +0200

    Player without lodash throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', onPlay);
+
+function onPlay() {
+    throttle(function (data) {
+      const time = data.seconds;
+      localStorage.setItem('videoplayer - current - time', time);
+    }, 1000);
+}
