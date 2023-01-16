dfed5187ef53eefa3b45e03e19f087a5128bdf89
helenserb <serbinenkoifbb@gmail.com>
2023-01-16 18:01:54 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector("input");
+const message = document.querySelector('textarea');
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 1000))
+
+populateInputs();
+
+function onInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateInputs() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(savedData);
+    if (savedData) {
+        if (savedData.email) {
+            email.value = savedData.email;
+        }
+        if (savedData.message) {
+            message.value = savedData.message;
+        }
+    }
+}
